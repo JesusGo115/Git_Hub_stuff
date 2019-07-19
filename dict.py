32b91ffad21803f1dcedf61a389a77c234c1d631
Ubuntu <ubuntu@ip-172-31-38-198.ec2.internal>
2019-07-19 18:03:13 +0000

    uhhhhhhh

--- dict.py
+++ dict.py
@@ -9,11 +9,6 @@ song = {
     "song_name": "Campfire song song"
 }
 
-LISTEN_TO_MY_MIXTAPE = {
-    "artist": "OOFER MCGOOBER",
-    "song_name": "Minecraftcito"
-}
-
 print_dict(list)
 
 print_dict(song)
